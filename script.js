17feb9cbfeb5078ced72705005b79c6a633a96db
sadoreena <78514743+sadoreena@users.noreply.github.com>
2026-07-03 00:10:02 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,97 @@
+const cardView = document.getElementById('card-view-btn')
+const listView = document.getElementById('list-view-btn')
+const mainElement = document.getElementById('view')
+
+listView.addEventListener('click', () => {
+  if (!mainElement.classList.contains('list-view')) {
+    mainElement.classList.add('list-view')
+  }
+  document.getElementById('list-view-btn').style.border = '1px solid white'
+  document.getElementById('card-view-btn').style.border = 'none'
+})
+
+cardView.addEventListener('click', () => {
+  mainElement.classList.remove('list-view')
+
+  document.getElementById('card-view-btn').style.border = '1px solid white'
+  document.getElementById('list-view-btn').style.border = 'none'
+})
+
+const businesses = [
+  // Business #1
+  {
+    name: 'BAKEHOUSE46',
+    photo_url: 'https://www.bakehouse46.com/wp-content/uploads/IMG_4393.jpg',
+    web_url: 'https://www.bakehouse46.com/locations/ferndale/'
+  },
+  // Business #2
+
+  {
+    name: 'Cookie Plug',
+    photo_url: 'https://cdn.shortpixel.ai/spai/w_1920+q_lossy+ret_img+to_webp/cookieplug.com/wp-content/uploads/2022/02/Plug-Catering-Background.jpg',
+    web_url: 'https://cookieplug.com/'
+  },
+  // Business #3
+
+  {
+    name: 'Detroit Cookie Co',
+    photo_url: 'https://www.detroitcookieco.com/wp-content/uploads/2022/04/GetYourCookieFix.png',
+    web_url: 'https://www.detroitcookieco.com/'
+  },
+  // Business #4
+  {
+    name: 'Quix Chocolate',
+    photo_url: 'https://static.wixstatic.com/media/57e146_5d1194099b0c43a7a79e1363bd72c4a7~mv2.jpg/v1/crop/x_0,y_586,w_5704,h_1957/fill/w_2872,h_982,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/220921_QuixChocolateGrandOpeningDetroit_A0320.jpg',
+    web_url: 'https://www.quixchocolate.com/'
+  },
+  // Business #5
+  {
+    name: 'Quickly Boba Cafe',
+    photo_url: 'https://scontent-det1-1.xx.fbcdn.net/v/t39.30808-6/476227714_1095155109289008_6716291326004238985_n.jpg?stp=dst-jpg_tt6&cstp=mx960x722&ctp=s960x722&_nc_cat=104&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=OfU8fEki4DsQ7kNvwG1iTZ-&_nc_oc=Adr5ep_k7j1LL_TxzcLW93yF8u3G7IcdtBqERumeNEi3Nx8swgHdyzAi5Ee1luiBhykeGwk96sPjZHT8XBbxK0GH&_nc_zt=23&_nc_ht=scontent-det1-1.xx&_nc_gid=mRPuamE27sl1kGHqqXw3eQ&_nc_ss=7b289&oh=00_AQBhbZpqHohUfdpAH4HsnLdWCN2AlO7x3BssqKr0eHwWsw&oe=6A4CFED7',
+    web_url: 'https://www.facebook.com/QuicklyBobaCafeFerndale/'
+  },
+  // Business #6
+  {
+    name: 'Western Market',
+    photo_url: 'https://cdn.prod.website-files.com/6959e829f2e0ba589605e0fe/69bc1d3e8bcb3167ffaa00e8_nourish-western-market-ferndale-025.avif',
+    web_url: 'https://www.westernmkt.com/'
+  },
+  // Business #7
+  {
+    name: 'Java Hutt Cafe',
+    photo_url: 'https://javahuttferndale.com/wp-content/uploads/2018/04/Mocha-and-Cookie.jpg',
+    web_url: 'https://javahuttferndale.com/'
+  },
+  // Business #8
+  {
+    name: 'Traverse Whiskey Co',
+    photo_url: 'https://www.tcwhiskey.com/wp-content/uploads/2023/08/Peach-Landing.jpg',
+    web_url: 'https://www.tcwhiskey.com/'
+  },
+  // Business #9
+  {
+    name: 'Urbanrest Brewing Co',
+    photo_url: 'https://www.urbanrest.com/wp-content/uploads/ZMS.jpg',
+    web_url: 'https://www.urbanrest.com/'
+  },
+  // Business #10
+  {
+    name: 'Valentine Distilling',
+    photo_url: 'https://images.squarespace-cdn.com/content/v1/654a9d64203002780b060312/f46e3936-a345-4eb8-9b16-def1cd8dede7/Valentine+Distilling+Co.+Still+Close-Up.jpg?format=2500w',
+    web_url: 'https://www.valentinedistilling.com/'
+  }
+]
+
+function createCard (business) {
+  return `
+    <div class="card">
+      <a target="_blank" href=${business.web_url}><img class="bus-img" src='${business.photo_url}'></img></a>
+      <div class="option-text">
+        <h1>${business.name}</h1>
+        <a id='bus-url' target="_blank" href=${business.web_url}>Visit the Website</a>
+      </div>
+    </div>
+  `
+}
+
+mainElement.innerHTML = businesses.map(createCard).join('')
